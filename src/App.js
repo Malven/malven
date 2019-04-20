5e5934ea73b5ee88c3e60b3533c55d9cac177872
Marcus <Malven@users.noreply.github.com>
2019-04-20 15:54:53 +0200

    Removed default files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Intent, H1 } from '@blueprintjs/core';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload!!!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="bp3-dark">
+				<H1>JOIN</H1>
+				<Button large fill intent={Intent.PRIMARY}>
+					JOIN
+				</Button>
+			</div>
+		);
+	}
 }
 
 export default App;
